9a40d818a3076876db68a71ce4550e88c29f0ee6
David Paul <shadow@Davids-MacBook-Pro.local>
2018-04-03 13:45:43 -0500

    inital commit

---
+++ main.js
@@ -0,0 +1,16 @@
+//Using Javascript, create a variable to hold your last name. 
+function lastNameResponder(){
+
+    const lastName = "Paul";
+    //Using the if-else statement
+    //do the following: 
+    //If your last name has 7 or more letters, console log "I can't pronounce this last name". 
+    //Else, console log your last name.
+    if (lastName.length >= 7){
+        console.log("I can't pronounce this last name");
+    }else {
+        console.log(lastName);
+    }
+    //Challenge: Create a new repository on Github and push the code you wrote up to Github.
+}
+lastNameResponder();
